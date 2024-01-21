6856fde8aafbf51d6aba01b25d61c79e37e1457d
jithin philip <jithinphilip347@gmail.com>
2024-01-21 21:46:21 +0530

    middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,8 @@
 import User from "../models/userModel.js";
 import bcryptjs from 'bcryptjs';
 
-export const signup =async (req , res) => {
+
+export const signup =async (req , res, next) => {
 
     const {username , email ,password} = req.body;
     const hashPassword = bcryptjs.hashSync(password , 10);
@@ -11,7 +12,7 @@ export const signup =async (req , res) => {
     await newUser.save()
     res.status(201).json("user created successfully!") 
     } catch (error) {
-        res.status(500).json(error.message)
+       next(error);
     }
 
    
